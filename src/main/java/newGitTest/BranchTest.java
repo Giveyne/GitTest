78a6628c395fa6bf094693ecb694b3f9e2ab0f39
Igor <criokondor@mail.ru>
2020-01-08 02:20:21 +0300

    Adding some code

--- src/main/java/newGitTest/BranchTest.java
+++ src/main/java/newGitTest/BranchTest.java
@@ -1,4 +1,20 @@
 package newGitTest;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Predicate;
+
+
 public class BranchTest {
+
+    public static void main(String[] args) {
+
+        List <String> list = new LinkedList<>(Arrays.asList("aaa", "aaa1", "bbb", "bbb1", "ccc", "ccc1"));
+
+       list
+               .stream()
+               .filter((s) -> s.startsWith("a"))
+               .forEach(System.out::println);
+    }
 }
